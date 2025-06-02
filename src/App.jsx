c8fe3adb8ca33b4d77796f5e7cc5e3e0a86d902d
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-06-02 12:17:45 +0100

    ajout de la première partis

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <main class="flex h-screen w-screen items-center justify-center bg-[url('https://i.imgur.com/1O8ovx1.jpeg')] bg-cover">
-
+      <div class="flex h-64 w-[26rem] flex-col rounded-3xl border-2 border-white/80 bg-gradient-to-br from-white/30 to-white/30 p-8 text-white shadow-lg backdrop-blur-lg">
+    <div class="flex w-full items-center">
+      <h2 class="mr-auto text-2xl font-bold">Mastercard</h2>
+      <img class="size-8" src="https://i.imgur.com/eLwgPd2.png" />
+    </div>
+    
+  </div>
     </main>
   )
 }
