ce607ae2ede4567eb73191b730c485dc50d641ae
Jordan Nsadisi <astro17nsadisi@gmail.com>
2025-06-02 12:33:39 +0100

    fix(bg): revue du lien

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    <main class="flex h-screen w-screen items-center justify-center bg-[url('https://i.imgur.com/1O8ovx1.jpeg')] bg-cover">
+    <main class="flex h-screen w-screen items-center justify-center bg-[url(https://i.imgur.com/1O8ovx1.jpeg)] bg-cover">
       <div class="flex h-64 w-[26rem] flex-col rounded-3xl border-2 border-white/80 bg-gradient-to-br from-white/30 to-white/30 p-8 text-white shadow-lg backdrop-blur-lg">
         <div class="flex w-full items-center">
           <h2 class="mr-auto text-2xl font-bold">Mastercard</h2>
